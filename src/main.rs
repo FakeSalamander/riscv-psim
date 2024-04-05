395936b47c1c674b08080f06c255895aca0ca18b
FakeSalamander <89767421+FakeSalamander@users.noreply.github.com>
2024-04-04 20:37:54 -0700

    code: addi test

--- src/main.rs
+++ src/main.rs
@@ -256,4 +256,50 @@ pub mod instr_tests {
         assert_eq!(state.reg_mem[4], ((4 as u32) << 12) + 12); //PC = 12
         assert_eq!(state.reg_mem[5], ((0 as u32) << 12) + 16); //PC = 16
     }
+
+    #[test]
+    fn smoke_test() {
+        // A single huge program with many varied instructions.
+        // Serves as a simple "are there bugs?" test, like a cheksum.
+        let instructions = Vec::<u32>::from([
+            0b00000000000100000000000010010011, //addi $r1, $r0, 1,
+            0b00000000001000000000000100010011, //addi $r2, $r0, 2
+            0b00000000011000010000000110010011, //addi $r3, $r2, 6
+            0b00000000001000000000001000010011, //addi $r4, $r0, 2
+            0b00000000000100000000001010010011, //addi $r5, $r0, 1
+        ]);
+
+        //CPU SETUP: Initializes the state and logic structs.
+        let mut state = Registers {
+            ifid: IFIDLatch::default(),
+            idex: IDEXLatch::default(),
+            exmem: EXMEMLatch::default(),
+            memwb: MEMWBLatch::default(),
+
+            pc: 0,
+
+            instr_mem: instructions,
+            reg_mem: vec![0; 32], //makes a vector of 32 zeroes.
+            data_mem: HashMap::new(),
+        };
+
+        let mut logic = Logic::default();
+
+        //ADDITIONAL SETUP:
+
+        //a vector of snapshots to make rewinding possible.
+        let mut backups: Vec<Snapshot> = Vec::new();
+
+        run_program(&mut state, &mut logic);
+
+        assert_eq!(state.reg_mem[0], 0);
+        assert_eq!(state.reg_mem[1], 1);
+        assert_eq!(state.reg_mem[2], 2);
+        assert_eq!(state.reg_mem[3], 8);
+        assert_eq!(state.reg_mem[4], 2);
+        assert_eq!(state.reg_mem[5], 1);
+        for i in 6..32 {
+            assert_eq!(state.reg_mem[i], 0);
+        }
+    }
 }
